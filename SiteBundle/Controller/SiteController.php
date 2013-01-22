7bec6040a1a4ca2f5d90b631d704d50fee16eb63
Joris BODIN <bodin.joris@gmail.com>
2013-01-22 17:02:21 +0100

    chut

--- SiteBundle/Controller/SiteController.php
+++ SiteBundle/Controller/SiteController.php
@@ -315,14 +315,16 @@ class SiteController extends Controller
 				$em->flush();
 				$nbsemaine = $semestre->nombreSemaines();
 				$date = $semestre->getDateDebut();
-				$date2 = $date;
+				$date2 =$semestre->getDateDebut();
 				
-				for ($i = 1; $i <=$nbsemaine+1; $i++) {					
-					$semaine = new Semaine($semestre, $i, $date,  $date2->add(new \DateInterval('P06D')));
+				for ($i = 1; $i <=$nbsemaine+1; $i++) {
+					$date2->add(new \DateInterval('P06D'));
+					$semaine = new Semaine($semestre, $i, $date,  $date2);
 					$sm = $this->getDoctrine()->getManager();
 					$sm->persist($semaine);
 					$sm->flush();
 					$date2->add(new \DateInterval('P01D'));
+					$date->add(new \DateInterval('P07D'));
 				}
 				echo "<script>alert(\"Le semestre a été ajouté avec ses semaines\")</script>";
 				}
